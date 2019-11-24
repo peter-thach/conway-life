8302e1f1cbb85656e70c7e1a73b3b85b33c3d11f
Peter Thach <petercthach@gmail.com>
2019-11-24 18:35:10 -0500

    delegated game logic to src/utils/Patterns.js

--- src/components/Game.js
+++ src/components/Game.js
@@ -3,6 +3,7 @@ import Grid from './Grid';
 import NavigationBar from './NavigationBar';
 import Legend from './Legend';
 import { selectPattern, setPattern } from '../utils/Patterns';
+import { gameLogic } from '../utils/gameLogic';
 
 class Game extends React.Component {
   constructor(props) {
@@ -129,85 +130,7 @@ class Game extends React.Component {
   }
   
   startGame() {
-    const loop = () => {
-      if(this.state.variant === 'danger') {
-          let neighbors = Array(30);
-          for(var i = 0; i < neighbors.length; i++) {
-            neighbors[i] = Array(79);
-            for(var j = 0; j < neighbors[i].length; j++) {
-              neighbors[i][j] = 0;
-            }
-          }
-
-          for(var i = 0; i < 30; i++) {
-            for(var j = 0; j < 79; j++) {
-              // do the corner first
-              if(i === 0 && j === 0) {
-                neighbors[i][j] = this.state.squares[i+1][j] + this.state.squares[i][j+1] + this.state.squares[i+1][j+1];
-              }
-              else if(i === 0 && j === neighbors[0].length - 1) {
-                neighbors[i][j] = this.state.squares[i][j-1] + this.state.squares[i+1][j-1] + this.state.squares[i+1][j];
-              }
-              else if(i === neighbors.length - 1 && j === 0) {
-                neighbors[i][j] = this.state.squares[i-1][j] + this.state.squares[i-1][j+1] + this.state.squares[i][j+1];
-              }
-              else if(i === neighbors.length - 1 && j === neighbors[0].length - 1) {
-                neighbors[i][j] = this.state.squares[i-1][j] + this.state.squares[i-1][j-1] + this.state.squares[i][j-1];
-              }
-              // now the edges
-              else if(i === 0) {
-                neighbors[i][j] = this.state.squares[i][j-1] + this.state.squares[i+1][j-1] + this.state.squares[i+1][j] + this.state.squares[i+1][j+1] + this.state.squares[i][j+1];
-              }
-              else if(i === neighbors.length - 1) {
-                neighbors[i][j] = this.state.squares[i][j-1] + this.state.squares[i-1][j-1] + this.state.squares[i-1][j] + this.state.squares[i-1][j+1] + this.state.squares[i][j+1];
-              }
-              else if(j === 0) {
-                neighbors[i][j] = this.state.squares[i-1][j] + this.state.squares[i-1][j+1] + this.state.squares[i][j+1] + this.state.squares[i+1][j+1] + this.state.squares[i+1][j];
-              }
-              else if(j === neighbors[0].length - 1) {
-                neighbors[i][j] =  this.state.squares[i-1][j] + this.state.squares[i-1][j-1] + this.state.squares[i][j-1] + this.state.squares[i+1][j-1] + this.state.squares[i+1][j];
-              }
-              // general case
-              else {
-                neighbors[i][j] = this.state.squares[i-1][j-1] + this.state.squares[i-1][j] + this.state.squares[i-1][j+1] + this.state.squares[i][j+1] + 
-                                  this.state.squares[i+1][j+1] + this.state.squares[i+1][j] + this.state.squares[i+1][j-1] + this.state.squares[i][j-1];
-              }
-            }
-          }
-
-          let newArr = Array(30);
-          for(var i = 0; i < newArr.length; i++) {
-            newArr[i] = Array(79);
-            for(var j = 0; j < newArr[i].length; j++) {
-              if(this.state.squares[i][j] == 1) {
-                if(neighbors[i][j] < 2) {
-                  newArr[i][j] = 0;
-                }
-                else if(neighbors[i][j] > 3) {
-                  newArr[i][j] = 0;
-                }
-                else {
-                  newArr[i][j] = 1;
-                }
-              }
-              else {
-                if(neighbors[i][j] == 3) {
-                  newArr[i][j] = 1;
-                }
-                else {
-                  newArr[i][j] = 0;
-                }
-              }
-            }
-          }
-          this.setState({squares: newArr});
-
-        if(this.state.variant === 'danger') {
-          setTimeout(loop, this.state.delay);
-        }
-      }
-    }
-    setTimeout(loop, this.state.delay);
+    gameLogic.call(this);
   }
 
   render() {
