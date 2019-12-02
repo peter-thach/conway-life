132270eec5306c62106f52db3ca90ef21128d64e
Peter Thach <petercthach@gmail.com>
2019-12-02 15:45:06 -0500

    Updates

---
+++ precache-manifest.fd676f5db5593abd70bd6093921f3163.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1fa7cb2453c4d3ff11d189bb7289055",
+    "url": "/conway-life/index.html"
+  },
+  {
+    "revision": "1acb61d1be48822ee4a5",
+    "url": "/conway-life/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "234386d8a30cf437713f",
+    "url": "/conway-life/static/css/main.c9796a34.chunk.css"
+  },
+  {
+    "revision": "1acb61d1be48822ee4a5",
+    "url": "/conway-life/static/js/2.168cacfc.chunk.js"
+  },
+  {
+    "revision": "234386d8a30cf437713f",
+    "url": "/conway-life/static/js/main.d71e5d3c.chunk.js"
+  },
+  {
+    "revision": "403556ac4bc8cdec0108",
+    "url": "/conway-life/static/js/runtime-main.e05de395.js"
+  }
+]);
